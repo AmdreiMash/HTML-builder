318c1de61a6b2ee86358b7726b2a2af7d270e46f
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-11-04 01:42:56 +0300

    fix: fix copyDir function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,19 @@
-const fsProm = require('fs/promises')
-const path = require('path')
-const getFiles = require('../03-files-in-folder/index')
+const fsProm = require('fs/promises');
+const path = require('path');
+const getFiles = require('../03-files-in-folder/index');
+
+
+
 
 async function copyDir() {
+  await fsProm.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true });
   try {
-
     const createDir = await fsProm.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
     console.log(`created ${(createDir || 'done')}`);
     const files = await getFiles('files', __dirname)
     files.forEach(async file => {
       try {
-        await fsProm.copyFile(path.join(__dirname,'files', file), path.join(__dirname, 'files-copy', file))
+        await fsProm.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file))
       } catch {
         console.log('The file could not be copied');
       }
@@ -20,9 +23,10 @@ async function copyDir() {
   }
 }
 
+
 copyDir()
 
-module.exports = getFiles;
+module.exports = { getFiles, copyDir };
 
 //! При выполнении этого скрипта будет так же отображен список файлов из 3-й части.
 //! Я экспортировал оттуда функцию, и почему-то воспроизводится весь скрипт :)
