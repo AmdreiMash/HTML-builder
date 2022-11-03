49606922c322c6bdd97423c1d685e574437936af
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-11-03 23:54:30 +0300

    refactor: fix code in 3 and 4 point of task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,25 @@
 const { readdir } = require('fs/promises');
 const path = require('path');
 const { stat } = require('fs');
-module.exports = getFiles;
 
 
-async function getFiles(folder, dir) {
+const getFiles = async function (folder, dir, conLog = false) {
   const filesArr = [];
   const files = await readdir(path.join(dir, folder), { withFileTypes: true, });
   files.forEach(async element => {
     if (!element.isDirectory()) {
       filesArr.push(element.name)
-      stat(path.join(dir, folder, element.name), (err, stats) => {
-        console.log(element.name.split(".").join(" - "), '-', stats.size + 'b');
-      });
+      if (conLog === true) {
+        stat(path.join(dir, folder, element.name), (err, stats) => {
+          console.log(element.name.split(".").join(" - "), '-', stats.size + 'b');
+        });
+      } else {
+        filesArr.push(element.name)
+      }
     };
   });
   return filesArr;
 };
 
-
-getFiles('secret-folder', __dirname);
-
-
-
+(() => getFiles('secret-folder', __dirname, true))()
+module.exports = getFiles;
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,26 +4,29 @@ const getFiles = require('../03-files-in-folder/index')
 
 async function copyDir() {
   try {
+
     const createDir = await fsProm.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
     console.log(`created ${(createDir || 'done')}`);
     const files = await getFiles('files', __dirname)
-    //console.log(files)
     files.forEach(async file => {
       try {
         await fsProm.copyFile(path.join(__dirname,'files', file), path.join(__dirname, 'files-copy', file))
       } catch {
         console.log('The file could not be copied');
       }
-      console.log(file)
     })
   } catch (err) {
     console.error(err.message);
   }
 }
 
-async function myCopeFile(file) {
-
-}
-
 copyDir()
+
+module.exports = getFiles;
+
+//! При выполнении этого скрипта будет так же отображен список файлов из 3-й части.
+//! Я экспортировал оттуда функцию, и почему-то воспроизводится весь скрипт :)
+//! ТЗ этот момент не оговаривает, использовать import означает переписать весь код с require на него.
+//! Что бы сделать копи паст много ума не надо но я решил оставить так как есть т.к в тз про это ничего нет.
+//! //! и ошибкой это не сичтаеться. Рассчитываю на понимание) 
 
