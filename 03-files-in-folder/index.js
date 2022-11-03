6c35ee35cf5a84a603d60304901c51bf9f5cf3d9
AmdreiMash <100795104+AmdreiMash@users.noreply.github.com>
2022-11-03 22:09:55 +0300

    feat: complate 04-copy-directory

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,25 @@
 const { readdir } = require('fs/promises');
-const path = require('path'); // <fs.Dirent>
-const { stat } = require('fs')
+const path = require('path');
+const { stat } = require('fs');
+module.exports = getFiles;
 
 
-async function getFiles() {
-  const files = await readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true, })
+async function getFiles(folder, dir) {
+  const filesArr = [];
+  const files = await readdir(path.join(dir, folder), { withFileTypes: true, });
   files.forEach(async element => {
     if (!element.isDirectory()) {
-      stat(path.join(__dirname, 'secret-folder', element.name), (err, stats) => {
-        console.log(element.name.split(".").join(" - "),'-',stats.size+'b');
-        return stats.size;
+      filesArr.push(element.name)
+      stat(path.join(dir, folder, element.name), (err, stats) => {
+        console.log(element.name.split(".").join(" - "), '-', stats.size + 'b');
       });
-    }
+    };
   });
-}
+  return filesArr;
+};
 
 
-getFiles()
+getFiles('secret-folder', __dirname);
+
+
+
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fsProm = require('fs/promises')
+const path = require('path')
+const getFiles = require('../03-files-in-folder/index')
+
+async function copyDir() {
+  try {
+    const createDir = await fsProm.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+    console.log(`created ${(createDir || 'done')}`);
+    const files = await getFiles('files', __dirname)
+    //console.log(files)
+    files.forEach(async file => {
+      try {
+        await fsProm.copyFile(path.join(__dirname,'files', file), path.join(__dirname, 'files-copy', file))
+      } catch {
+        console.log('The file could not be copied');
+      }
+      console.log(file)
+    })
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function myCopeFile(file) {
+
+}
+
+copyDir()
 
